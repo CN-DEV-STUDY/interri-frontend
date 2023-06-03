9cf15290925032968b30b7b8236d79b1473478cf
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-06-03 13:51:39 +0900

    충돌 해결

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,4 @@
 import {
-<<<<<<< HEAD
   ROUTES_ROOT_MAIN,
   ROUTES_DESIGN_REQUEST_REGISTRATION,
   ROUTES_ROOT_HOME,
@@ -38,40 +37,6 @@ const router = createBrowserRouter([
     path: ROUTES_DESIGN_REQUEST_REGISTRATION,
     element: <DesignRequestRegistration />,
   },
-=======
-    ROUTES_ROOT_MAIN,
-    ROUTES_DESIGN_REQUEST_REGISTRATION,
-    ROUTES_ROOT_HOME,
-    ROUTES_DESIGN_REQUEST_DETAILS,
-} from "@/routes/routes";
-import { createBrowserRouter } from "react-router-dom";
-import App from "@/App";
-import Home from "@/components/Home";
-import DesignReqDetail from "@/container/designReq/DesignReqDetail";
-import DesignReqRegist from "@/components/DesignReqRegist";
-
-const router = createBrowserRouter([
-    {
-        path: ROUTES_ROOT_MAIN,
-        element: <App />,
-        // errorElement: <ErrorBoundary />,
-        children: [],
-    },
-    {
-        path: ROUTES_ROOT_HOME,
-        element: <Home />,
-        children: [
-            {
-                path: ROUTES_DESIGN_REQUEST_DETAILS,
-                element: <DesignReqDetail />,
-            },
-        ],
-    },
-    {
-        path: ROUTES_DESIGN_REQUEST_REGISTRATION,
-        element: <DesignReqRegist />,
-    },
->>>>>>> 7224bb4405411d15f65a7f10fb246e0ca3591a22
 ]);
 
 export default router;
