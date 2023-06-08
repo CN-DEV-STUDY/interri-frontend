3cb48f8e47bca815168a18eb7d4a86d6bf3a10ac
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-06-08 22:45:14 +0900

    로그인 상태에 따른 사용자 메뉴 분기처리

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -1,71 +1,83 @@
-import userState from '@/global/user/userState'
-import { ROUTES_LOGIN, ROUTES_SIGN_UP } from '@/routes/routes'
-import { Link } from 'react-router-dom'
-import { useSetRecoilState } from 'recoil'
-import styled from 'styled-components'
+import userState from "@/global/user/userState";
+import { ROUTES_LOGIN, ROUTES_SIGN_UP } from "@/routes/routes";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useRecoilState, useResetRecoilState } from "recoil";
+import styled from "styled-components";
 
 type LinkProps = {
-	onClick?: () => void
-}
+  onClick?: () => void;
+};
 /**
  * @description 유저 메뉴 컴포넌트
  */
 const UserMenu = () => {
-	// state
-	const setUserState = useSetRecoilState(userState)
+  // global
+  const [userStateValue, setUserState] = useRecoilState(userState);
+  const { isLoggedIn } = userStateValue;
 
-	const onLogout = () => {
-		console.log('logout')
-		setUserState({ isLoggedIn: false })
-	}
+  console.log("isLoggedIn >>>> ", isLoggedIn);
 
-	// view
-	return (
-		<Wrap>
-			<ProfileLink to={ROUTES_LOGIN}>로그인</ProfileLink>
-			<ProfileLink to={'#'}>알림</ProfileLink>
-			<ProfileLink to={'#'}>메세지</ProfileLink>
-			<ProfileLink to={'#'}>마이페이지</ProfileLink>
-			<ProfileLink to={ROUTES_SIGN_UP}>회원가입</ProfileLink>
-			<ProfileLink to={'#'} onClick={onLogout}>
-				로그아웃
-			</ProfileLink>
-		</Wrap>
-	)
-}
+  const onLogout = () => {
+    console.log("logout");
+    setUserState({ isLoggedIn: false });
+  };
 
-export default UserMenu
+  // view
+  return (
+    <Wrap>
+      {!isLoggedIn ? (
+        <>
+          <ProfileLink to={ROUTES_LOGIN}>로그인</ProfileLink>
+          <ProfileLink to={ROUTES_SIGN_UP}>회원가입</ProfileLink>
+        </>
+      ) : (
+        <>
+          <ProfileLink to={"#"}>알림</ProfileLink>
+          <ProfileLink to={"#"}>메세지</ProfileLink>
+          <ProfileLink to={"#"}>마이페이지</ProfileLink>
+          <ProfileLink to={"#"} onClick={onLogout}>
+            로그아웃
+          </ProfileLink>
+        </>
+      )}
+    </Wrap>
+  );
+};
+
+export default UserMenu;
 
 // STYLED COMPONENTS
 const Wrap = styled.div`
-	width: 150px;
-	height: 320px;
+  width: 150px;
+  height: 320px;
+  padding-top: 20px;
 
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	gap: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 30px;
 
-	position: absolute;
-	border-radius: 9px;
+  position: absolute;
+  border-radius: 9px;
 
-	z-index: 1; /*맨 앞으로 가져오기 */
-	top: 50%;
-	left: 20%;
-	transform: translateY(10%);
+  z-index: 1; /*맨 앞으로 가져오기 */
+  top: 50%;
+  left: 20%;
+  transform: translateY(10%);
 
-	background-color: #f8f9fa;
-	border: 1px solid #e7e8e9;
-`
+  background-color: #f8f9fa;
+  border: 1px solid #e7e8e9;
+`;
 
 const ProfileLink = styled(Link)<LinkProps>`
-	font-size: 20px;
-	text-decoration: none;
+  font-size: 20px;
+  text-decoration: none;
 
-	&:hover,
-	&:active {
-		text-decoration: underline dotted red;
-		color: red;
-	}
-`
+  &:hover,
+  &:active {
+    text-decoration: underline dotted red;
+    color: red;
+  }
+`;
