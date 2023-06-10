faac3f73062bfc16cbc8e2a56042cc4645f8231b
chaesuhyeon <ddd8177@gmail.com>
2023-06-10 23:04:04 +0900

    CN-40 디자인 요청 상세보기 화면 개발 진행중

--- src/components/design/designReq/DesignReqInfoContent.tsx
+++ src/components/design/designReq/DesignReqInfoContent.tsx
@@ -1,41 +1,56 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 interface GridItemProps {
-  mt?: number;
-  size?: number;
+    mt?: number;
+    size?: number;
 }
 
-function DesignReqInfoContent() {
-  return (
-    <Grid>
-      <GridTextItem size={44} mt={150}>
-        안방
-      </GridTextItem>
-      <GridImgItem src={process.env.PUBLIC_URL + "/images/design-req1.jpg"} />
-      <GridTextItem size={24}>
-        현관을 들어서는 순간 눈앞에 펼쳐진 멋진 바다뷰!! 플로리스트의 예쁜
-        꽃들이 함께하는 힐링공간!!!아침에 눈뜨면 햇살가득한 오션 테라스가 상쾌한
-        아침을 맞아주는 마법같은 뷰 맛집 ! (사진에 대한 설명)
-      </GridTextItem>
-    </Grid>
-  );
+interface ReqInfoDetailResources {
+    infoId?: number; // 개별 요청 내용 id
+    roomTypeNm?: string; // 공간
+    content?: string; // 요청 내용
+    imgPathList?: string[]; // 이미지 경로
+}
+
+interface Props {
+    info?: ReqInfoDetailResources;
+}
+
+function DesignReqInfoContent({ info }: Props) {
+    return (
+        <Grid>
+            <GridTextItem size={44} mt={150}>
+                {info?.roomTypeNm}
+            </GridTextItem>
+            {info?.imgPathList?.map((img, idx) => (
+                <GridImgItem
+                    key={idx.toString()}
+                    src={process.env.PUBLIC_URL + '/images/design-req1.jpg'}
+                    // src={img}
+                />
+            ))}
+            <GridTextItem size={24}>{info?.content}</GridTextItem>
+        </Grid>
+    );
 }
 
 export default DesignReqInfoContent;
 
 const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
+    display: grid;
+    grid-template-columns: 1fr;
 
-  row-gap: 30px;
+    row-gap: 30px;
 `;
 
 const GridTextItem = styled.div<GridItemProps>`
-  font-size: ${(props) => props.size}px;
-  margin-top: ${(props) => props.mt}px;
+    font-size: ${(props) => props.size}px;
+    margin-top: ${(props) => props.mt}px;
 `;
 
 const GridImgItem = styled.img`
-  width: 100%;
-  height: 800px;
+    width: 100%;
+    height: 800px;
 `;
